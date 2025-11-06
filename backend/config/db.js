757c2d3b13278515f04af48100c80e4e02db85d0
Muralikrishn123 <23071a66c7@vnrvjiet.in>
2025-11-06 22:26:23 +0530

    Update MongoDB connection to use MONGODB_URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/yourdbname', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI , {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
